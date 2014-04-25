8b37b513a8d629e0d584f7dc8ba7807b8b291d37
Rich <richard.abdill@gmail.com>
2014-04-24 23:53:19 -0400

    If excerpt is whole post, no "read more" link
    Mini post thing! Hooray!

--- blog/home.php
+++ blog/home.php
@@ -9,9 +9,18 @@
         $data=mysql_query("SELECT * FROM posts ORDER BY date DESC");
         while($info = mysql_fetch_array($data))
             {
-            echo "<h2 class='headline'><a href='post.php?postID=" . $info['postID'] . "'>" . $info['headline'] . "</a></h2>";  
+            $fullPost=true;
+                if($info['text'] == $info['excerpt']) $fullPost=false;  //  If the excerpt text is the entire post
+            echo "<h2 class='headline'>";
+                if($fullPost) echo"<a href='post.php?postID=" . $info['postID'] . "'>" . $info['headline'] . "</a></h2>";
+                else echo $info['headline'] . "</h2>";
             echo "<p class='datestamp'>" . date('F j, Y',strtotime($info['date'])) . "</p>";        
-            echo "<div class='article'>" . $info['excerpt'] . " <em><a href='post.php?postID=" . $info['postID'] . "'>Read more...</a></em></p></div>";                                           
+            echo "<div class='article'>" . $info['excerpt'] . " <em><a href='post.php?postID=" . $info['postID'] . "'>";
+                if($fullPost) echo"Read more...</a></em></p></div>";  
+                else echo "</p></div>";
+            
+            
+                                                    
                     //      *********** The excerpt ends with a "</p> because each excerpt should leave an unclosed <p> tag at the end, so the "Read more...." can be appended.******************
             }
 ?>
